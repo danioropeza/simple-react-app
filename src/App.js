ca26c0c07f8b0b06a6fe1e6434a4dd9496885f33
Jose Daniel Oropeza Soria <danioropezasoria@gmail.com>
2021-09-20 14:35:15 -0400

    Cambio de grupo 2.

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          new change g2 qwe
+          new change g2 123
         </a>
       </header>
     </div>
